7b80b9ed73df6797afe4485c66f2a6e0cd8282b8
2Kelvin <rocketmancodes02@gmail.com>
2023-01-11 08:10:19 +0300

    second commit

---
+++ app.js
@@ -0,0 +1,6 @@
+export default function App() {
+  return;
+}
+
+var root = ReactDOM.createRoot(document.getElementById("rootNode"));
+root.render(React.createElement(App, null));
---
+++ src/app.js
@@ -0,0 +1,6 @@
+export default function App() {
+  return;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("rootNode"));
+root.render(<App />);
